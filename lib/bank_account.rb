3dc48f13f2287bd9706b54dd8a2df1eb756b444b
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-23 00:08:29 -0400

    commit tree ae917af4726f513671e05378b010b629e63328a4

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
 
+    attr_accessor :bank_account, :balance, :status
+    attr_reader :name
+
+    def initialize(name, balance = 1000, status = "open")
+        @bank_account = bank_account
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(money)
+        @balance += money
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        @balance > 0 && @status == "open"
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
+
 end
